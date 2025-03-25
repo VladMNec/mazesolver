b3f7b5fb997bf34d8280984b757e478427227732
VM_N <necvladm@gmail.com>
2025-03-25 09:17:47 +0200

    add graphic classes

---
+++ main.py
@@ -0,0 +1,47 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.geometry(f"{width}x{height}")
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def draw_line(self, line, fill_colour):
+        line.draw(self.__canvas, fill_colour)
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+
+    def close(self):
+        self.__running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, a, b):
+        self.point_a = a
+        self.point_b = b
+
+    def draw(self, canvas, fill_colour):
+        canvas.create_line(self.point_a.x, self.point_a.y, self.point_b.x, self.point_b.y, fill=fill_colour, width=2)
+
+
+win = Window(800, 600)
+win.draw_line(Line(Point(50, 60), Point(240, 130)), "red")
+win.draw_line(Line(Point(124, 543), Point(720, 480)), "black")
+win.wait_for_close()
